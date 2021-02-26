6705f320e00aa41f908233346108c327f2d90d6f
pmqueiroz <pedroqueiroz593@gmail.com>
2021-02-26 09:35:58 -0300

    refactor: remove logs and unused vars

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,10 @@
 import { Container, Link } from './styles';
 import { FiPlus } from 'react-icons/fi';
 import { useRouter } from 'next/router';
-import Button from '../button';
 
 export default function Header () {
    const router = useRouter();
 
-   console.log(router.pathname);
-   
    return (
       <Container>
          <img src="logo.svg" alt="pmqueiroz"/>
